ae8f5afb6f46f3422858a362b653fef231a83a84
Alihussain121472 <121472+Alihussain121472@users.noreply.github.com>
2026-08-26 22:22:00 +0500

    perf: implement postgres connection pooling and globally redesign admin sidebar to modern enterprise dark mode

--- database.py
+++ database.py
@@ -12,8 +12,37 @@ class DummyDictCursor:
 
 
 
+import threading
+from psycopg2.pool import SimpleConnectionPool
+
+_pool = None
+_pool_lock = threading.Lock()
+
+class PooledConnection:
+    def __init__(self, conn, pool):
+        self._conn = conn
+        self._pool = pool
+    def cursor(self, *args, **kwargs):
+        return self._conn.cursor(*args, **kwargs)
+    def commit(self):
+        self._conn.commit()
+    def rollback(self):
+        self._conn.rollback()
+    def close(self):
+        try:
+            self._pool.putconn(self._conn)
+        except:
+            pass
+
 def safe_connect():
-    return psycopg2.connect(os.getenv('DATABASE_URL'))
+    global _pool
+    if _pool is None:
+        with _pool_lock:
+            if _pool is None:
+                db_url = os.getenv('DATABASE_URL')
+                _pool = SimpleConnectionPool(1, 30, db_url)
+    return PooledConnection(_pool.getconn(), _pool)
+
 import os
 import json
 from datetime import datetime, timedelta
